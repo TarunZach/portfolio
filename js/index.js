99ed8d8c91d6e9c6542d73b6298f9a7b4fdbad1b
TarunZach <tzacharias.97@gmail.com>
2025-01-24 12:17:27 +0100

    milestone: base template

--- js/index.js
+++ js/index.js
@@ -10,3 +10,14 @@ navLinks.forEach(link => {
     document.body.classList.remove('nav-open');
   })
 })
+
+/*Marquee*/
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
